2023b4785db8a0c48199f5f811ea27227adb6d55
Tanmay Jha <tanmay.jha1@gmail.com>
2018-10-09 00:16:31 +0530

    update php file

--- test.php
+++ test.php
@@ -269,7 +269,6 @@ function popup(source){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 1000, function(){
-   
         window.location.hash = hash;
       });
     } 
